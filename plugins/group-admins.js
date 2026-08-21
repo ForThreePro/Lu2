629c4590996cc38d9af1db7ab8f57cc8a2d1def1
YALLICO OWNER <yallico2024@gmail.com>
2026-08-21 18:50:44 -0500

    Actualizar group-admins.js

--- plugins/group-admins.js
+++ plugins/group-admins.js
@@ -1,6 +1,6 @@
 const handler = async (m, { conn, command }) => {
   if (!m.mentionedJid[0] &&!m.quoted) {
-    let texto = `🐱 𓆩 ***𝗚𝗔𝗥𝗙𝗜𝗘𝗟𝗗 𝗕𝗢𝗧 𝗢𝗙𝗜𝗖𝗜𝗔𝗟*** 𓆪 🐱
+    let texto = `🐱 𓆩 ***𝗟𝗨 𝗕𝗢𝗧 𝗣𝗥𝗘𝗠*** 𓆪 🐱
 
 *Uso:*
 .${command} @user → Para ${command === 'promote' || command === 'promover' || command === 'daradmin'? 'promover' : 'degradar'}
@@ -14,7 +14,7 @@ const handler = async (m, { conn, command }) => {
   let action = command === 'promote' || command === 'promover' || command === 'daradmin'? 'promote' : 'demote'
 
   let msgAccion = action === 'promote'
- ? `🐱 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗣𝗥𝗢𝗠𝗢𝗩𝗜𝗗𝗢 𓆪 🐱
+? `🐱 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗣𝗥𝗢𝗠𝗢𝗩𝗜𝗗𝗢 𓆪 🐱
 
 .⃟𖥔 ݁. 𖦹˙— \`\`PROMOTE\`\` —˙𖦹.🍕꒷
 
@@ -22,7 +22,7 @@ const handler = async (m, { conn, command }) => {
 😼 *Por:* @${m.sender.split('@')[0]}
 
 ━━━━━━━━━━━
-*Powered by*: ***Garfield Bot Oficial*** 🍕`
+*Powered by*: ***Lu Bot Prem*** 🍕`
     : `🐱 𓆩 𝗨𝗦𝗨𝗔𝗥𝗜𝗢 𝗗𝗘𝗚𝗥𝗔𝗗𝗔𝗗𝗢 𓆪 🐱
 
 .⃟𖥔 ݁. 𖦹˙— \`\`DEMOTE\`\` —˙𖦹.🍕꒷
@@ -31,7 +31,7 @@ const handler = async (m, { conn, command }) => {
 🍕 *Por:* @${m.sender.split('@')[0]}
 
 ━━━━━━━━━━━
-*Powered by*: ***Garfield Bot Oficial*** 🍕`
+*Powered by*: ***Lu Bot Prem*** 🍕`
 
   await m.react(action === 'promote'? '👑' : '📉')
   await conn.groupParticipantsUpdate(m.chat, [user], action)
